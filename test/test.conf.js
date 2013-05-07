10b9edd73e8c161cdaa3ae94d38aaa697797b68a
Brian Hann <emailc0bra@gmail.com>
2013-05-07 14:53:02 -0500

    Add coverage, fix jquery-ui component line

--- test/test.conf.js
+++ test/test.conf.js
@@ -4,12 +4,16 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'components/jquery/jquery.js',
-  'components/jquery-ui/ui/jquery-ui.custom.js',
+  'components/jquery-ui/ui/jquery-ui.js',
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
   'src/bootstrap-growl.js',
   'test/*.spec.js'
 ];
+
+preprocessors = {
+  'src/bootstrap-growl.js': 'coverage'
+};
 
 singleRun = true;
 
